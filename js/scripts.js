9b4dddba09dcfae0b145f48f54bab5e0e8fa473b
pivendanila <piven.danila@mail.ru>
2024-08-08 12:33:15 +0300

    Updated

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,62 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    emailjs.init('iTmnt56qxGAKGQzEF');
+
+    const formPopup = document.getElementById('formPopup');
+    const openFormBtns = document.querySelectorAll('.open-form-btn');
+    const contactForm = document.getElementById('contactForm');
+
+    openFormBtns.forEach(button => {
+                button.addEventListener('click', function() {
+                    formPopup.style.display = 'block';
+                    document.body.classList.add('overlay');
+                });
+            });
+
+    formPopup.addEventListener('click', function(event) {
+        if (event.target === formPopup) {
+            closeForm();
+        }
+    });
+
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+
+        const formData = new FormData(contactForm);
+        const vacancy = formData.get('vacancy');
+        const name = formData.get('name');
+        const experience = formData.get('experience');
+        const age = formData.get('age');
+        const number = formData.get('number');
+
+        const templateParams = {
+            vacancy: vacancy,
+            name: name,
+            experience: experience,
+            age: age,
+            number: number
+        };
+
+
+        const serviceID = 'service_mo4c1tn';
+        const templateID = 'template_y66jsmm';
+
+        emailjs.send(serviceID, templateID, templateParams).then(
+            (response) => {
+                alert('Сообщение отправлено!');
+                closeForm();
+            }, (error) => {
+                console.log('Ошибка отправки:', error);
+            }
+        );
+    });
+
+    function closeForm() {
+        formPopup.style.display = 'none';
+        document.body.classList.remove('overlay');
+    }
+});
